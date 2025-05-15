8045fd2dae5abc9b9cae6200b4b3e59b4828a73b
srinadila <srinadila398@gmail.com>
2025-05-15 20:41:20 +0700

    perbarui

---
+++ database/migrations/2025_05_15_132129_penjualan.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('custom_user', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->enum('role', ['owner', 'admin', 'pembeli']);
+            $table->timestamps();
+        });  
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->decimal('price', 10, 2);
+            $table->integer('stock');
+            $table->timestamps();
+        });
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('custom_user')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('custom_user')->onDelete('cascade');
+            $table->decimal('total_price', 10, 2);
+            $table->enum('status', ['pending', 'paid', 'shipped', 'completed']);
+            $table->timestamps();
+        });
+        Schema::create('transaction_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('transaction_id')->constrained('transactions')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->decimal('subtotal', 10, 2);
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('custom_user');
+        Schema::dropIfExists('products');
+        Schema::dropIfExists('carts');
+        Schema::dropIfExists('transactions');
+        Schema::dropIfExists('transaction_items');
+    }
+};
